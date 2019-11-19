813f1c66002915e9cca0d0818a7fdf3ea195c94d
Chandrashekar Marishetty.51641516 <chandrashekar_m@hcl.com>
2019-11-19 14:04:44 +0530

    chip producer completed

---
+++ src/main/java/com/fedex/ground/chip/ChipApplication.java
@@ -0,0 +1,15 @@
+package com.fedex.ground.chip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChipApplication.class, args);
+		System.out.println("this is swetha second application");
+
+	}
+
+}
